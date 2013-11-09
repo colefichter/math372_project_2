52a1bd5b4bed793cc716a32f25211088e68759e4
RahmatSaeedi <rahmat@ualberta.ca>
2013-11-09 15:05:27 -0700

    Simple Model
    I don't know how to use R :-(

--- simple_model.R
+++ simple_model.R
@@ -1,4 +1,4 @@
 #Plotting the Simple Litter model
 t <- seq(0,50,length=50*20)
-y <- 400*exp(0.6*t) - 400
-plot(t,y,type="l",lwd=2,col="red",ylab="x(t)")
+y <- 600-600*exp(-0.4*t)
+plot(t,y,type="l",lwd=2,col="red",xlab="Time (years)",ylab="Grams of C per Square Meter,   x(t)")
