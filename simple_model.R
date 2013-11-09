463fbc4a759cb8556f272729212094a536ed5fc8
Cole Fichter <cole.fichter@gmail.com>
2013-11-09 15:31:15 -0700

    Adding fancy plot of simple solution

--- simple_model.R
+++ simple_model.R
@@ -1,4 +1,5 @@
 #Plotting the Simple Litter model
 t <- seq(0,50,length=50*20)
 y <- 600-600*exp(-0.4*t)
-plot(t,y,type="l",lwd=2,col="red",xlab="Time (years)",ylab="Grams of C per Square Meter,   x(t)")
+plot(t,y,type="l",lwd=2,bty="n",col="red",xlab="Time (years)",ylab="",main="Grams of Carbon per Square Meter")
+text(39,550, labels="x(t)=600-600e^(-0.4t)")
